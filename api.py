255679e70cf965434aa16cf0f53106f15e4c59fd
Teja sri <73563207+Teja-veeramachaneni@users.noreply.github.com>
2022-02-18 20:55:46 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,34 @@
+import json,re
+from flask import Flask
+app = Flask(__name__)
+Data = """
+{
+    "details":[
+        {
+
+           "place" : "Alankar@theatre",
+           "contact Number": "7867567898"
+        },
+
+        {
+           "place" : "PostOffice",
+           "contact Number" : "08656244601"
+        }
+    ]
+    
+}"""
+res = json.loads(Data)
+@app.route("/")
+def result():
+    return 'Welcome!'
+@app.route("/v1/sanitized/input/")
+def ans():
+    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+    for i in res['details']:
+        if i['place'].isalpha():
+            return 'sanitized'
+        else:
+            return 'Unsanitized'
+
+
+app.run(port = 800)
